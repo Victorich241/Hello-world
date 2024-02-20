62eff45de0f848165fa39cd6bac82bb5e33d56da
Victorich241 <158127555+Victorich241@users.noreply.github.com>
2024-02-20 11:05:22 +0300

    Add files via upload
    ENC211-0067/2023

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("My name is Victor!\n");
+    printf("My course is civil!\n")
+    printf("My favorite meal is sushi!\n")
+     printf("My best team is Manchester city!\n")
+
+}
